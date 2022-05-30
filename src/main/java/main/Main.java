e7f834c52341422b5fbbcc224aefba486c19b17e
AndroidAndreevich <heonito@mail.ru>
2022-05-30 22:05:08 +0300

    HW1

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,31 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.AllRequestsServlet;
+import servlets.MirrorRequestsServlet;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+        MirrorRequestsServlet mirrorRequestsServlet = new MirrorRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorRequestsServlet), "/mirror");
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8081);
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+}
